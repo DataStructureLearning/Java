5da23f30b40c2e23f9942d2c459ddf47bdffab95
Hiem Seyha <seyha@3948348394398934.local>
2017-11-07 06:43:12 +0700

    selection sort

--- src/com/company/sort/SelectionSort.java
+++ src/com/company/sort/SelectionSort.java
@@ -3,22 +3,27 @@ package com.company.sort;
 /**
  * Created by seyha on 11/5/17.
  */
+public class SelectionSort {
 
-
-public class BubbleSort {
-
-    public static void BubbleSort(int[] arr) {
+    public static void SelectionSort(int[] arr) {
 
         int size = arr.length;
         int temp = 0;
 
         for (int i =0;i<size-1;i++) {
-            for(int j=1;j<size-i;j++) {
-                if(arr[j-1] > arr[j]) {
-                    temp = arr[j-1];
-                    arr[j-1] = arr[j];
-                    arr[j] = temp;
+
+            int index = i;
+
+            for(int j=i+1;j<size-1;j++) {
+                if(arr[j] < arr[index]) {
+                    index = j;
                 }
+            }
+
+            if(index != i) {
+                temp = arr[index];
+                arr[index] = arr[i];
+                arr[i] = temp;
             }
 
         }
@@ -28,7 +33,7 @@ public class BubbleSort {
     public static void main(String[] args) {
 
         int[] arr = {1,50,30,10,60,80};
-        System.out.println("Before bubble sort");
+        System.out.println("Before Selection sort");
 
 
         for(int i=0;i<arr.length-1;i++) {
@@ -36,9 +41,9 @@ public class BubbleSort {
         }
 
 
-        System.out.println("");
-        System.out.println("After bubble sort");
-        BubbleSort(arr);
+
+        System.out.println("\nAfter Selection sort");
+        SelectionSort(arr);
 
         for(int i=0;i<arr.length-1;i++) {
             System.out.print(arr[i] + "\t");
